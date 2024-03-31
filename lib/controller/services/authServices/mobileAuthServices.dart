95530fd9344faee4002478e01ac05fe9a4e9bb10
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-04-01 01:24:55 +0530

    add: Push Notifications Setup

--- lib/controller/services/authServices/mobileAuthServices.dart
+++ lib/controller/services/authServices/mobileAuthServices.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 import 'package:quicknoms/constant/constant.dart';
 import 'package:quicknoms/controller/provider/authProvider/authProvider.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:quicknoms/controller/services/pushNotificationServices/pushNotificationServices.dart';
 import 'package:quicknoms/view/authScreens/mobileLoginScreen.dart';
 import 'package:quicknoms/view/authScreens/otpScreen.dart';
 import 'package:quicknoms/view/bottomNavigationBar/bottomNavigationBar.dart';
@@ -88,7 +89,7 @@ class MobileAuthServices {
         value.size > 0 ? userIsRegistered = true : userIsRegistered = false;
         log('User is Registered = $userIsRegistered');
         if (userIsRegistered) {
-          //PushNotificationServices.initializeFCM();
+          PushNotificationServices.initializeFCM();
           Navigator.pushAndRemoveUntil(
             context,
             PageTransition(
